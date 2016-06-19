3f0d21391c033def9f6e8deda325dfed6f2e1faf
Yelzifang <348288001@qq.com>
2016-06-20 07:39:35 +0800

    2016-6-20

--- Supermarket/WebContent/js/main.js
+++ Supermarket/WebContent/js/main.js
@@ -13,7 +13,8 @@ function sure1(){
 	var csure1 = document.getElementById("staffadd");
 	document.getElementById("staffadd").style.display="none";
 }
-/*取消添加框*/
+
+/*取消员工添加框*/
 function display1(){
 	var cdisplay1 = document.getElementById("staffadd");
 	document.getElementById("staffadd").style.display="none";
@@ -21,5 +22,70 @@ function display1(){
 /*删除员工信息*/
 function display2(){
 	var cshow = document.getElementById("showstaff");
+	document.getElementById("showstaff").style.display="none";
+}
+/*删除类别信息*/
+function display31(){
+	var csho = document.getElementById("ss_print");
+	document.getElementById("ss_print").style.display="none";
+}
+/*确定类别修改框*/
+function sure311(){
+	var cshfwo1 = document.getElementById("sortchange");
+	document.getElementById("sortchange").style.display="none";
+}
+/*取消类别修改框*/
+function display311(){
+	var cshfwo = document.getElementById("sortchange");
+	document.getElementById("sortchange").style.display="none";
+}
+/*确定类别添加框*/
+function sure312(){
+	var cshf1wo1 = document.getElementById("sortadd");
+	document.getElementById("sortadd").style.display="none";
+}
+/*取消类别添加框*/
+function display312(){
+	var cshfwro = document.getElementById("sortadd");
+	document.getElementById("sortadd").style.display="none";
+}
+/*确定商品修改框*/
+function sure42(){
+	var cshewfwo1 = document.getElementById("comesschange");
+	document.getElementById("comesschange").style.display="none";
+}
+/*取消商品修改框*/
+function display42(){
+	var rt = document.getElementById("comesschange");
+	document.getElementById("comesschange").style.display="none";
+}
+/*确定商品添加框*/
+function sure41(){
+	var cshlof1wo1 = document.getElementById("comessadd");
+	document.getElementById("comessadd").style.display="none";
+}
+/*取消商品添加框*/
+function display41(){
+	var wro = document.getElementById("comessadd");
+	document.getElementById("comessadd").style.display="none";
+}
+/*删除商品信息*/
+function display3(){
+	var cshow3 = document.getElementById("printlight");
+	document.getElementById("printlight").style.display="none";
+}
+/*确定进货框*/
+function sure51(){
+	var cshlof1wo1 = document.getElementById("amountadd");
+	document.getElementById("amountadd").style.display="none";
+}
+/*取消进货框*/
+function display51(){
+	var wro = document.getElementById("amountadd");
+	document.getElementById("amountadd").style.display="none";
+}
+/*删除进货记录*/
+function display511(){
+	var wep = document.getElementById("showstaff");
 	document.getElementById("showstaff").style.display="none";
 }
